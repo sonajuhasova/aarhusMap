4c4738c17efbc7f1f8d6d6261825eeb733fd1c9e
Sona <sona.juhasova@gmail.com>
2020-03-05 21:37:51 +0100

    map works on hover, no rotation

---
+++ script.js
@@ -0,0 +1,100 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29uYWp1aGFzb3ZhIiwiYSI6ImNrN2QxNmt1YjA1aWIza3FrY3F6cmEyZnAifQ.yQnjU9hjmTyVa7hbbo2z-A';
+var map = new mapboxgl.Map({
+    style: 'mapbox://styles/sonajuhasova/ck7d24w5p0ckv1ipou5dgwqu6',
+    center: [10.204, 56.140],
+    zoom: 12,
+    pitch: 45,
+    bearing: -17.6,
+    container: 'map',
+    antialias: true
+});
+
+// The 'building' layer in the mapbox-streets vector source contains building-height
+// data from OpenStreetMap.
+map.on('load', function () {
+    // Insert the layer beneath any symbol layer.
+    var layers = map.getStyle().layers;
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addLayer({
+            'id': '3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // use an 'interpolate' expression to add a smooth transition effect to the
+                // buildings as the user zooms in
+                'fill-extrusion-height': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'height']
+],
+                'fill-extrusion-base': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'min_height']
+],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+
+
+    //            // fly to an object
+
+    $('#aarhus').hover(function () {
+        map.flyTo({
+            center: [10.204, 56.140],
+            zoom: 12,
+
+
+        })
+    })
+
+    $('#church').hover(function () {
+        map.flyTo({
+            center: [10.212, 56.157],
+            zoom: 17,
+
+
+        })
+    })
+
+    $('#aros').hover(function () {
+        map.flyTo({
+            center: [10.202, 56.154],
+            zoom: 17,
+
+
+        })
+    })
+
+    $('#garden').hover(function () {
+        map.flyTo({
+            center: [10.195, 56.161],
+            zoom: 17,
+
+
+        })
+    })
+}); //the end
